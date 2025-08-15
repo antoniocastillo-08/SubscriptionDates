0d99a86211385ea0d006b1939936dfe0b8bcd12d
Antonio Castillo <antoniocj04@gmail.com>
2025-08-15 21:53:30 +0200

    Starting project

---
+++ app/Http/Controllers/SuscriptionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSuscriptionRequest;
+use App\Http\Requests\UpdateSuscriptionRequest;
+use App\Models\Suscription;
+
+class SuscriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSuscriptionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Suscription $suscription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Suscription $suscription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSuscriptionRequest $request, Suscription $suscription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Suscription $suscription)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/elpepe', function () {
+    return 'elpepe';
+});
